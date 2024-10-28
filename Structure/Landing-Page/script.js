dd1faf3d68af785adfce095c2a2dca4c714baa6a
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2024-10-28 13:55:22 +0800

    Finished the portfolio

--- Structure/Landing-Page/script.js
+++ Structure/Landing-Page/script.js
@@ -1,6 +1,12 @@
+animateName();
+animationNoEnd();
 
 function animationNoEnd(){
    $("#windyl").fadeOut(1000).fadeIn(1000)
    animationNoEnd();
 }
-animationNoEnd();
+
+function animateName(){
+   $("#myname").fadeOut(1000).fadeIn(1000);
+     
+}
