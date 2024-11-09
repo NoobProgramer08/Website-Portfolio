d38ae98e53957dc1049c9d671db3e3d69618806f
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2024-11-09 16:16:16 +0800

    modified the file paths and arrange the files

--- Structure/Landing-Page/script.js
+++ Structure/Landing-Page/script.js
@@ -2,11 +2,11 @@ animateName();
 animationNoEnd();
 
 function animationNoEnd(){
-   $("#windyl").fadeOut(1000).fadeIn(1000)
+   $("#windyl").fadeOut(1000).fadeIn(1000);
    animationNoEnd();
-}
 
+}
 function animateName(){
    $("#myname").fadeOut(1000).fadeIn(1000);
-     
+   
 }
